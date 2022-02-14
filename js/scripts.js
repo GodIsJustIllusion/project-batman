ece611a5752ed75d07343bea7389a44d2ebf015a
Igor_A <87186964+GodIsJustIllusion@users.noreply.github.com>
2022-02-14 22:54:00 +0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    spaceBetween: 0,
+    spaceBetween: 30,
     
     // Optional parameters
     loop: true,
@@ -10,9 +9,38 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.main-slider__wrap'
     },
     breakpoints: {
-        480: {
+        320: {
+          slidesPerView: 1,
+        },
+        768: {
           slidesPerView: 2,
-          spaceBetween: 30,
         }
-    },
+    },  
   });
+
+  let burger = document.querySelector ('.menu-burger');
+  let closeMenu = document.querySelector ('.header__menu-close');
+  let menuMobile = document.querySelector ('.header');
+
+  burger.addEventListener ('click', () => {
+      menuMobile.style.display = 'block';
+  })
+
+  closeMenu.addEventListener ('click', () => {
+    menuMobile.style.display = 'none';
+  })
+
+  const modalWindow = document.querySelector('.modal');
+  const buttonModal = document.querySelector('.main__button-play');
+
+  buttonModal.addEventListener ('click', (e) => {
+      modalWindow.classList.add('active');
+  })
+
+  modalWindow.addEventListener('click', (e) => {
+      const isModal = e.target.closest('.modal__inner');
+
+      if (!isModal) {
+        modalWindow.classList.remove('active');
+      }
+  })
